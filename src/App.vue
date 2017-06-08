9e0068542e388c077a872163d0b46deb6e5b1f56
Suiyang <171426589@qq.com>
2017-06-08 13:53:13 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script src="main.js"></script>
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style lang="scss">
+  * {
+    padding: 0;
+    border: 0;
+    margin: 0;
+  }
+  body,html{
+    height: 100%;
+    width: 100%;
+  }
+  #app {
+    height: 100%;
+    width: 100%;
+  }
+</style>
